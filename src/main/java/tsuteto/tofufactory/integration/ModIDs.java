68100f4b7cf6ac409a57467fa0cc08fc1c186512
yoruki_167 <78334210+167OSS@users.noreply.github.com>
2025-12-09 16:23:13 +0800

    Update ModIDs.java

--- src/main/java/tsuteto/tofufactory/integration/ModIDs.java
+++ src/main/java/tsuteto/tofufactory/integration/ModIDs.java
@@ -1,13 +1,11 @@
 package tsuteto.tofufactory.integration;
 
-public class ModIDs
-{
+public class ModIDs {
+
     public static final String BC = "BuildCraft|Core";
     public static final String FFM = "Forestry";
     public static final String IC2 = "IC2";
     public static final String FC = "FarmCraftory";
-    public static final String GT5 = "gregtech";
-    public static final String GT6 = "gregapi";
     public static final String MT = "mod_ecru_MapleTree";
     public static final String Bamboo = "BambooMod";
     public static final String AE = "appliedenergistics2";
