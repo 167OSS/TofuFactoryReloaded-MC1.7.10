35f7e32029b9992461a2fa62330c327628cb919c
yoruki_167 <78334210+167OSS@users.noreply.github.com>
2025-12-09 16:18:25 +0800

    Add files via upload

---
+++ src/main/java/tsuteto/tofufactory/bee/TFBeeClassification.java
@@ -0,0 +1,73 @@
+package tsuteto.tofufactory.bee;
+
+import java.util.ArrayList;
+
+import forestry.api.genetics.AlleleManager;
+import forestry.api.genetics.IAlleleSpecies;
+import forestry.api.genetics.IClassification;
+
+public enum TFBeeClassification implements IClassification {
+
+    TOFU("Tofu"),
+    VEGETABLE("Vegetable");
+
+    private String uID;
+    private String latin;
+    private ArrayList<IAlleleSpecies> species;
+    private IClassification parent;
+    private EnumClassLevel level;
+
+    TFBeeClassification(String scientific) {
+        this.uID = "classification." + this.name()
+            .toLowerCase();
+        this.latin = scientific;
+        this.level = EnumClassLevel.GENUS;
+        this.species = new ArrayList<IAlleleSpecies>();
+        this.parent = AlleleManager.alleleRegistry.getClassification("family.apidae");
+        AlleleManager.alleleRegistry.registerClassification(this);
+    }
+
+    public EnumClassLevel getLevel() {
+        return this.level;
+    }
+
+    public String getUID() {
+        return this.uID;
+    }
+
+    public String getName() {
+        return this.getUID();
+    }
+
+    public String getScientific() {
+        return this.latin;
+    }
+
+    public String getDescription() {
+        return this.getUID() + ".description";
+    }
+
+    public IClassification[] getMemberGroups() {
+        return null;
+    }
+
+    public void addMemberGroup(IClassification group) {}
+
+    public IAlleleSpecies[] getMemberSpecies() {
+        return this.species.toArray(new IAlleleSpecies[this.species.size()]);
+    }
+
+    public void addMemberSpecies(IAlleleSpecies species) {
+        if (!this.species.contains(species)) {
+            this.species.add(species);
+        }
+    }
+
+    public IClassification getParent() {
+        return this.parent;
+    }
+
+    public void setParent(IClassification parent) {
+        this.parent = parent;
+    }
+}
