95b5a58ad876bc3bd601c9ee4a963b4783cad097
yoruki_167 <78334210+167OSS@users.noreply.github.com>
2025-12-09 16:25:20 +0800

    Update TofuMachineRecipe.java

--- src/main/java/tsuteto/tofufactory/machinerecipe/TofuMachineRecipe.java
+++ src/main/java/tsuteto/tofufactory/machinerecipe/TofuMachineRecipe.java
@@ -1,35 +1,23 @@
 package tsuteto.tofufactory.machinerecipe;
 
 import net.minecraft.item.ItemStack;
-import tsuteto.tofufactory.api.recipes.RecipeManagers;
-import tsuteto.tofufactory.integration.TFIntegrationManager;
-import tsuteto.tofufactory.integration.plugins.PluginGreg5;
-import tsuteto.tofufactory.integration.plugins.PluginGreg6;
 
-public class TofuMachineRecipe
-{
-    public static void initRecipe()
-    {
+import tsuteto.tofufactory.api.recipes.RecipeManagers;
+
+public class TofuMachineRecipe {
+
+    public static void initRecipe() {
         RecipeManagers.pulverizerManager = new PulverizerRecipe();
         RecipeManagers.compactorManager = new CompactorRecipe();
         RecipeManagers.cuttingMachineManager = new CuttingMachineRecipe();
     }
 
-    public static ItemStack getOreDictStack(ItemStack aOreStack)
-    {
-        if (TFIntegrationManager.modGT6.isReady())
-        {
-            return PluginGreg6.getOreDictStack(aOreStack.copy());
-        }
-        else if (TFIntegrationManager.modGT5.isReady())
-        {
-            return PluginGreg5.getOreDictStack(aOreStack.copy());
-        }
-        else
-        {
-            return aOreStack;
-        }
+    /**
+     * Returns a copy of the original stack.
+     * GT5 / GT6 compatibility removed.
+     */
+    public static ItemStack getOreDictStack(ItemStack stack) {
+        if (stack == null) return null;
+        return stack.copy();
     }
-
-
 }
